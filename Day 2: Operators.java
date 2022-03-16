a8816ba22f755c3ad34b25ff1e0b403914938678
Ahmed Gamal <ahmedgemy2212@gmail.com>
2022-03-16 17:12:04 +0200

    Days 2:3

--- Day 2: Operators.java
+++ Day 2: Operators.java
@@ -12,15 +12,6 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'solve' function below.
-     *
-     * The function accepts following parameters:
-     *  1. DOUBLE meal_cost
-     *  2. INTEGER tip_percent
-     *  3. INTEGER tax_percent
-     */
-
     public static void solve(double meal_cost, int tip_percent, int tax_percent) {
     // Write your code here
         System.out.println(Math.round(meal_cost + Double.valueOf(tip_percent) / 100 * meal_cost 
--- Day 3: Intro to Conditional Statements.java
+++ Day 3: Intro to Conditional Statements.java
@@ -27,7 +27,6 @@ public class Solution {
         }else if(n > 20){
             System.out.println("Not Weird");
         }
-        
         bufferedReader.close();
     }
 }
